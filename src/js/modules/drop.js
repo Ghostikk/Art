a1ed4fb28c017f1fb9620672f89a16db01f3f473
Vladimir Seledkin <vladimyr_seledkin@mail.ru>
2022-08-23 20:25:23 +0300

    Реализация задач согласно плана task.txt

--- src/js/modules/drop.js
+++ src/js/modules/drop.js
@@ -1,41 +1,78 @@
 const drop = () => {
-  // События при драг & дроп функциональности: drag*, dragend*, dragenter - объект над dropArea, dragexit*, dragleave - объект за пределами dropArea, dragover - объект в пределах dropArea, dragstart*, drop - объект "упал" в dropArea (* - события у элементов, которые перетаскивают)
-    const fileInputs = document.querySelectorAll('[name="upload"]');
-  // свой массив сробытий
-    ['dragenter', 'dragleave', 'dragover', 'drop'].forEach (eventName => {
-        fileInputs.forEach (input => {
-            input.addEventListener (eventName, preventDefault, false);
+    // drag *
+    // dragend *
+    // dragenter - объект над dropArea
+    // dragexit *
+    // dragleave - объект за пределами dropArea
+    // dragover - объект зависает над dropArea
+    // dragstart *
+    // drop - объект отправлен в dropArea
+    const fileInputs = document.querySelectorAll('[name="upload"]'),
+          btnTop = document.querySelector(".calc .file_upload > button"),
+          btnMidlle = document.querySelector('.popup-design .file_upload>button'),
+          btnCalc = document.querySelector('.col-md-3 .file_upload>button');
+
+    // создаем свой массив событий
+    ['dragenter', 'dragleave', 'dragover', 'drop'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, preventDefaults, false);
         });
     });
-    // отмена стандартного поведения
-    function preventDefault (e) {
+
+    //отменяем стандартное поведение и всплытие событий
+    function preventDefaults(e) {
         e.preventDefault();
         e.stopPropagation();
     }
-    // подсвечивания элементов
-    function highlight (item) {
-      //возвращает ближайший родительский элемент (или сам элемент), который соответствует заданному CSS-селектору 
-        item.closest('.file_upload').style.border = '5px solid yellow';
-        item.closest('.file_upload').style.backgroundColor = 'rgba(0,0,0, .7)';
+    //добавляем подсветку контура для дропа
+    function highlight(item) {
+        // closest возвращает ближайший родительский элемент (или сам элемент), который соответствует заданному CSS-селектору
+        item.closest('.file_upload').style.border = "2px dashed black";
+        item.closest('.file_upload').style.backgroundColor = "#f7e7e6";
+        btnTop.style.border = "none";
+        btnMidlle.style.border = "none";
+        btnCalc.style.border = "none";
+
     }
-    // убираем подсветку с элементов
-     function unHighlight (item) {
-        item.closest('.file_upload').style.border = 'none';
-        item.closest('.file_upload').style.backgroundColor = '#ededed';
+     //отменяем подсветку
+    function unhighlight(item) {
+        item.closest('.file_upload').style.border = "none";
+        btnTop.style.border = "2px dashed #c51abb";
+        btnMidlle.style.border = "2px dashed #c51abb";
+        btnCalc.style.border = "2px dashed #c51abb";
+        // родитель с классом .calc_form
+        if (item.closest('.calc_form')) {
+            item.closest('.file_upload').style.backgroundColor = "#fff";
+        } else if (item.closest('.col-md-3')) {
+            item.closest('.file_upload').style.backgroundColor = "#f7e7e6";
+        } else {
+            item.closest('.file_upload').style.backgroundColor = "#ededed";
+        }
     }
 
-    ['dragenter', 'dragover'].forEach (eventName => {
-        fileInputs.forEach (input => {
-            input.addEventListener (eventName, () => highlight(input), false);
-        });
-    });
-  
-    ['dragleave', 'drop'].forEach (eventName => {
-        fileInputs.forEach (input => {
-            input.addEventListener (eventName, () => unHighlight(input), false);
+    ['dragenter', 'dragover'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, () => highlight(input), false);
         });
     });
 
+    ['dragleave', 'drop'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, () => unhighlight(input), false);
+        });
+    });
+
+  fileInputs.forEach(input => {
+      input.addEventListener('drop', (e) => {
+          input.files = e.dataTransfer.files;
+          let dots;
+          const arr = input.files[0].name.split('.');
+
+          arr[0].length > 6 ? dots = "..." : dots = '.';
+          const name = arr[0].substring(0, 6) + dots + arr[1];
+          input.previousElementSibling.textContent = name;
+      });
+  });
 };
 
 export default drop;
--- src/js/modules/scrolling.js
+++ src/js/modules/scrolling.js
@@ -9,10 +9,10 @@ const scrolling = (upSelector) => {
             upElem.classList.remove('fadeIn');
         }
     });
+    
     // Scrolling with RAF
-
     let links = document.querySelectorAll('[href^="#"]'),
-        speed = 0.5;
+        speed = 0.25;
 
     links.forEach(element => {
         element.addEventListener('click', function(event) {
@@ -31,7 +31,8 @@ const scrolling = (upSelector) => {
                 }
 
                 let progress = time - start,
-                    pixels = (toBlock < 0 ? Math.max(widthTop - (progress/speed), widthTop + toBlock) : Math.min(widthTop + (progress/speed), widthTop + toBlock));
+                    pixels = (toBlock < 0 ? Math.max(widthTop - (progress/speed), widthTop + toBlock) :
+                               Math.min(widthTop + (progress/speed), widthTop + toBlock));
                 
                 document.documentElement.scrollTo(0, pixels);
 
@@ -45,25 +46,20 @@ const scrolling = (upSelector) => {
     });
 
     //Pure JS Scrolling
-
     // const element = document.documentElement,
     //       body = document.body;
+    
     // const calcScroll = () => {
     //     upElem.addEventListener('click', function(e) {
-            
     //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
-
     //         if(this.hash !== '') {
     //             let hashElement = document.querySelector(this.hash),
     //                 hashElementTop = 0;
-                
     //             while(hashElement.offsetParent) {
     //                 hashElementTop += hashElement.offsetTop;
     //                 hashElement = hashElement.offsetParent;
     //             }
-
     //             hashElementTop = Math.round(hashElementTop);
-
     //             smoothScroll(scrollTop, hashElementTop, this.hash);
     //         }
     //     });
@@ -71,12 +67,9 @@ const scrolling = (upSelector) => {
 
     // const smoothScroll = (from, to, hash) => {
     //     let timeInterval = 1, prevScrollTop, speed;
-
     //     to > from ? speed = 30 : speed = -30;
-
     //     let move = setInterval (function() {
     //         let scrollTop = Math.round(body.scrollTop || element.scrollTop);
-            
     //         if(
     //             prevScrollTop === scrollTop ||
     //             (to > from && scrollTop >= to) ||
@@ -91,7 +84,6 @@ const scrolling = (upSelector) => {
     //         }
     //     }, timeInterval);
     // };
-
     // calcScroll();
 };
 
